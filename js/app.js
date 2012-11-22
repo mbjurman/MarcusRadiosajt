d4f88c78ddabc833848eeead4a2c6fc579b8e41d
Marcus Bjurman <mbjurman@gmail.com>
2012-11-22 23:12:57 +0100

    :wq

--- js/app.js
+++ js/app.js
@@ -1,8 +1,19 @@
 /**************************
 * Application
 **************************/
-App = Em.Application.create();
+var App = Em.Application.create({
+	rootContainer: null,
+	player: null,
+	ready: function() {
+		App.rootContainer.appendTo("#channel-list-area");
+		App.rootContainer.set('currentView', App.ChannelListView.create());
+		App.player = document.getElementById("audio-player");
+	}
+});
 
+App.rootContainer = Ember.ContainerView.create({
+	childViews: []
+});
 
 /**************************
 * Models
@@ -18,8 +29,13 @@ App.Channel = Em.Object.extend({
 /**************************
 * Views
 **************************/
-App.channelView = Ember.View.extend({
-	
+App.ChannelListView = Ember.View.extend({
+	templateName: 'channel-list',	
+	content: null
+})
+
+App.ChannelView = Ember.View.extend({
+	templateName: 'channel-list-item',	
 	content: null,
 	show: function(event, view){
 		var url = this.content.url;
@@ -39,40 +55,20 @@ App.channelsController = Em.ArrayController.create({
 		var url = 'http://sverigesradio.se/api/v2/channels?pagination=false&format=json&callback=?';
 		$.getJSON(url,function(data){
 			me.set('content', []);
-			$(data).each(function(index,value){
-			    $(value.channels).each(function(index2,value2){
-					var t = App.Channel.create({
-						name: value2.name,
-						imageurl: value2.image,
-						category: value2.channeltype,
-						url: value2.liveaudio.url
-					});
-					if (!Ember.empty(t.imageurl) && t.category !== "Lokal kanal" && t.category !== "Extrakanaler")
-					{
-						me.pushObject(t);
-					}
-				})
+			$(data.channels).each(function(index,value) {
+				var t = App.Channel.create({
+					name: value.name,
+					imageurl: value.image,
+					category: value.channeltype,
+					url: value.liveaudio.url
+				});
+				if (!Ember.empty(t.imageurl) && t.category !== "Lokal kanal" && t.category !== "Extrakanaler")
+				{
+					me.pushObject(t);
+				}
 			})
 		});
     }
 });
 
-App.streamingUrlsController = Em.ArrayController.create({
-    content: [],
-    loadUrls: function(channel) {
-        var me = this;
-        me.clear();
-        $(channel.streamingurls).each(function(index, value) {
-            me.pushObject(value);
-        })
-    }
-});
-
-App.playerMetadata = Em.Object.create({
-	title: "",
-	imageurl: null
-})
-
 App.channelsController.loadChannels();
-App.player = document.getElementById("audio-player");
-
